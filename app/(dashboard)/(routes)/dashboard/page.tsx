48c23d72b1fab3cc30d56f7ab7c820bc37c18f30
ALW-N <officialalwintomy@gmail.com>
2024-05-26 22:42:40 +0530

    SideBar/ commit-5

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,12 @@
 const DashboardPage = () => {
   return (
     <div>
-      <p>DashBoard Page Protected</p>
+      <div className="mb-8 apce-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center ">
+          Explore thepower of AI
+        </h2>
+        <p className="text-muted-foreground">Chat with the smarttest AI </p>
+      </div>
     </div>
   );
 };
