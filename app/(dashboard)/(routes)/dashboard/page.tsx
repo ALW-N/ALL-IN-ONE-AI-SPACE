1ee47dedce8fd833121cd3e64bd39effb009702d
ALW-N <officialalwintomy@gmail.com>
2024-05-27 23:34:46 +0530

    Dashboard/ commit-2

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -67,7 +67,7 @@ const DashboardPage = () => {
           <Card
             onClick={() => router.push(tool.href)}
             key={tool.href}
-            className="p-4 border-black/5 items-center justify-between hover:shadow-md transition cursor-pointer"
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
           >
             <div className="flex items-center gap-x-4">
               <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
@@ -75,6 +75,7 @@ const DashboardPage = () => {
               </div>
               <div className="font-semibold">{tool.label}</div>
             </div>
+            <ArrowRight className="w-8 h-8" />
           </Card>
         ))}
       </div>
